0b09a824e303ffe0362f02581d0c4fbbfc9fbc6c
nphtu <nphoangtu@gmail.com>
2020-11-13 14:03:21 +0700

    demo git push

--- src/main/java/spm/project/restaurantrecommendation/controller/LoginController.java
+++ src/main/java/spm/project/restaurantrecommendation/controller/LoginController.java
@@ -1,14 +1,16 @@
 package spm.project.restaurantrecommendation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import spm.project.restaurantrecommendation.service.UserService;
 
 
 @Controller
 public class LoginController {
 
-
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/home")
     public String getIndexPage() {
