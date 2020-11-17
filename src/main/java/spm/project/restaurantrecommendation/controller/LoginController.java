15cee32ae240b9bfecfd1762d03b796f91aaec77
nphtu <nphoangtu@gmail.com>
2020-11-17 18:02:28 +0700

    fix login controller

--- src/main/java/spm/project/restaurantrecommendation/controller/LoginController.java
+++ src/main/java/spm/project/restaurantrecommendation/controller/LoginController.java
@@ -1,16 +1,12 @@
 package spm.project.restaurantrecommendation.controller;
 
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
-=======
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
->>>>>>> staging
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import spm.project.restaurantrecommendation.entity.User;
 import spm.project.restaurantrecommendation.service.UserService;
-
 import java.util.List;
 
 import java.security.Principal;
@@ -19,49 +15,56 @@ import java.security.Principal;
 @Controller
 public class LoginController {
 
-<<<<<<< HEAD
+
     @Autowired
     private UserService userService;
 
-    @GetMapping("/home")
+    @GetMapping("/admin/home")
     public String getIndexPage() {
         return "index";
-=======
-
-    @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
-    @GetMapping({ "/" })
-    public String getIndexPage(Principal principal, Authentication authentication) {
-        return "index-test";
->>>>>>> staging
-    }
-    @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
-    @GetMapping("/login")
-    public String login(){ return "login"; }
-
-    @GetMapping("/login-test")
-    public String getLoginPage() {
-        return "login-test";
     }
 
-    @GetMapping("/resetpassword")
-    public String getResetPasswordPage() { return "resetpass"; }
+        @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
+        @GetMapping({"/"})
+        public String getIndexPage (Principal principal, Authentication authentication){
+            return "index-test";
 
-    @GetMapping("/registration-test")
-    public String getRegistrationTestPage() {
-        return "registration-test";
-    }
+        }
+        @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
+        @GetMapping("/login")
+        public String login () {
+            return "login";
+        }
 
-<<<<<<< HEAD
+        @GetMapping("/login-test")
+        public String getLoginPage () {
+            return "login-test";
+        }
+
+        @GetMapping("/resetpassword")
+        public String getResetPasswordPage () {
+            return "resetpass";
+        }
+
+        @GetMapping("/registration-test")
+        public String getRegistrationTestPage () {
+            return "registration-test";
+        }
+
+
 //    @GetMapping("/users")
 //    public List<User> getAllUser() { return userService.findAll(); }
-=======
-    @GetMapping("/admin/index")
-    public String getAdminPage() {
-        return "admin/index";
-    }
-    @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
-    @GetMapping("/403")
-    public String get403Page() {  return "403"; }
 
->>>>>>> staging
+        @GetMapping("/admin/index")
+        public String getAdminPage () {
+            return "admin/index";
+        }
+
+        @PreAuthorize("!(hasRole('USER') OR hasRole('ADMIN'))")
+        @GetMapping("/403")
+        public String get403Page () {
+            return "403";
+        }
+
+
 }
