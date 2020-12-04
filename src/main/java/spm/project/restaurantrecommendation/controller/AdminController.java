eee038b79b1437a4aa9a83043b2b539326077cec
nhatquang99 <vhnhatquang7399@gmail.com>
2020-12-04 23:48:06 +0700

    admin update user

--- src/main/java/spm/project/restaurantrecommendation/controller/AdminController.java
+++ src/main/java/spm/project/restaurantrecommendation/controller/AdminController.java
@@ -8,10 +8,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import spm.project.restaurantrecommendation.dto.UserUpdateInfoDto;
 import spm.project.restaurantrecommendation.entity.Role;
 import spm.project.restaurantrecommendation.entity.User;
 import spm.project.restaurantrecommendation.service.RestaurantService;
@@ -19,6 +18,7 @@ import spm.project.restaurantrecommendation.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.security.Principal;
 import java.text.Normalizer;
 import java.util.ArrayList;
@@ -57,22 +57,54 @@ public class AdminController {
         return "admin/index";
     }
 
-    @GetMapping("/admin/edit-user-{id}")
-    public String showAdminUpdate(Model model, @PathVariable long id, Principal principal, HttpServletRequest request, HttpServletResponse response){
+    @GetMapping("/admin/edit-user/{id}")
+    public String showAdminUpdate(Model model,
+                                  @PathVariable long id,
+                                  Principal principal,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response, Authentication authentication){
         if (principal == null) {
             return "redirect:/admin";
         }
 
+        if (authentication != null) {
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            List<String> roles = new ArrayList<String>();
+            for (GrantedAuthority a : authorities) {
+                roles.add(a.getAuthority());
+            }
+            if (!isAdmin(roles)) {
+                return "/403";
+            }
+        }
+
         User u = userService.findById(id);
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (auth != null) {
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//        }
 
         model.addAttribute("user", u);
 
         return "admin/update";
+    }
+
+    @PostMapping("/admin/edit-user/{id}")
+    public String updateUserInfo(Authentication authentication,
+                                 @PathVariable long id,
+                                 @ModelAttribute("user") @Valid UserUpdateInfoDto userUpdateInfoDto,
+                                 BindingResult result,
+                                 Principal principal) {
+
+        if (authentication == null) return "redirect:/admin";
+
+        if (result.hasErrors()) return "redirect:/admin";
+
+        userService.save(userUpdateInfoDto);
+        userService.autoLogin(principal.getName());
+        return "redirect:/admin";
+
     }
 
 
