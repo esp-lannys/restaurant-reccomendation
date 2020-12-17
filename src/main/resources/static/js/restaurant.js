8293336010e289a77b56a1698ca5f100624b5030
nhatquang99 <vhnhatquang7399@gmail.com>
2020-12-17 21:56:53 +0700

    validate form reservation

--- src/main/resources/static/js/restaurant.js
+++ src/main/resources/static/js/restaurant.js
@@ -1,9 +1,21 @@
-function saveData() {
-	const tableSize = Number($('#tableSize').val());
-	const date = $('#date').val();
-	const time = $('#time').val();
+$('#form').submit(function() {
+		if ($('#tableSize').val() == '') {
+			$('#notification').text('Please input your table size');
+			return false;
+		} else if ($('#date').val() == '') {
+			$('#notification').text('Please input your date');
+			return false;
+		} else if ($('#time').val() == '') {
+			$('#notification').text('Please input your time');
+			return false;
+		} else {
+			const tableSize = Number($('#tableSize').val());
+            const date = $('#date').val();
+            const time = $('#time').val();
 
-	window.localStorage.setItem("tableSize", tableSize);
-	window.localStorage.setItem("time", time);
-	window.localStorage.setItem("date", date);
-}
+            window.localStorage.setItem("tableSize", tableSize);
+            window.localStorage.setItem("time", time);
+            window.localStorage.setItem("date", date);
+	        return true;
+		}
+})
